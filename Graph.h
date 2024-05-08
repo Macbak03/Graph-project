0a50b15b5f14f02a7a0a23ade8e4f777e6dce4e6
Kacper <kacper.mumot1@gmail.com>
2024-05-08 19:59:50 +0200

    FIX

--- Graph.h
+++ Graph.h
@@ -2,12 +2,12 @@
 #include <string>
 #include <vector>
 
-
 class Graph {
 private:
     int nodesAmount{};
     std::vector<std::vector<int>> matrix;
     std::vector<std::pair<int, int>> table;
+    std::vector<std::vector<int>> consequentsList;
 
     void createGraphUser();
     void createGraphGenerate(int saturation);
@@ -17,19 +17,14 @@ private:
     void createTableFromList();
     void createTableFromMatrix();
 public:
-    std::vector<std::vector<int>> consequentsList;
     const static std::string argumentUser;
     const static std::string argumentGenerated;
 
     explicit Graph(const std::string& argument);
     void handleType(const std::string &argument);
-    
-    int getSize();
 
     std::string type;
     std::vector<std::vector<int>>* getList();
     std::vector<std::vector<int>>* getMatrix();
     std::vector<std::pair<int, int>>* getTable();
-    
-    std::vector<int> getNeighbors(int node);
 };
